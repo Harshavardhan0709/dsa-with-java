3d9e48307f1b948256172f67f917112cc4dca4d5
Harsha <k.harsha0719@gmail.com>
2026-01-01 12:12:21 +0530

    Remove compiled .class files and add gitignore

--- 2.Arrays/SecondLargest.java
+++ 2.Arrays/SecondLargest.java
@@ -1,27 +1,26 @@
 public class SecondLargest {
     public static int getSecondLargest(int numbers[]) {
         int firstLargest = Integer.MIN_VALUE;
-        int secondLargest=Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > firstLargest) {
-                secondLargest=firstLargest;
+                secondLargest = firstLargest;
                 firstLargest = numbers[i];
-            }else if(numbers[i]>secondLargest && numbers[i]!=firstLargest) //checking for duplicates
-                {
-                secondLargest=numbers[i];
+            } else if (numbers[i] > secondLargest && numbers[i] != firstLargest) // checking for duplicates
+            {
+                secondLargest = numbers[i];
             }
         }
         return secondLargest;
     }
 
     public static void main(String[] args) {
-        int numbers[] = { -1, 2, 3, 4, 10, 67,67, 19, -10 };
-        int numbers2[] = { -10, 0, -30, -35};
-        int numbers3[] = { -10, -30, -35};
-        System.out.println("Largest number in array is: " + getSecondLargest(numbers));
-        System.out.println("Largest number in array is: " + getSecondLargest(numbers2));
-        System.out.println("Largest number in array is: " + getSecondLargest(numbers3));
+        int numbers[] = { -1, 2, 3, 4, 10, 67, 67, 19, -10 };
+        int numbers2[] = { -10, 0, -30, -35 };
+        int numbers3[] = { -10, -30, -35 };
+        System.out.println("Second Largest number in array is: " + getSecondLargest(numbers));
+        System.out.println("Second Largest number in array is: " + getSecondLargest(numbers2));
+        System.out.println("Second Largest number in array is: " + getSecondLargest(numbers3));
     }
 }
-
