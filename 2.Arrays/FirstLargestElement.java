3372f32c9ea88d4aec3f883d0f12b04a1d39648e
Harsha <k.harsha0719@gmail.com>
2026-01-01 12:37:56 +0530

    Day 2: Arrays – linear search, largest, smallest, second largest

--- 2.Arrays/FirstLargestElement.java
+++ 2.Arrays/FirstLargestElement.java
@@ -12,9 +12,12 @@ public class FirstLargestElement {
     public static void main(String[] args) {
         int numbers[] = { -1, 2, 3, 4, 10, 67, 19, -10 };
         int numbers2[] = { -10, 0, -30, -35};
-        int numbers3[] = { -10, -30, -35};
+        int numbers3[] = { -10, -30, -35,-36};
+        int numbers4[]={100,-200,1000,200,30,40};
         System.out.println("Largest number in array is: " + getFirstLargest(numbers));
         System.out.println("Largest number in array is: " + getFirstLargest(numbers2));
         System.out.println("Largest number in array is: " + getFirstLargest(numbers3));
+        System.out.println("Largest number in array is: " + getFirstLargest(numbers4));
+        
     }
 }
