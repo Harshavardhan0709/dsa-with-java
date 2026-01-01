0c72ad7b580ea660fcecc37e74ec4027596b74cb
Harsha <k.harsha0719@gmail.com>
2026-01-01 12:34:17 +0530

    Day 1: Basics – variables, conditionals, loops, functions

--- 1.Basics/Loops/Mulitiplication.java
+++ 1.Basics/Loops/Mulitiplication.java
@@ -5,9 +5,10 @@ public class Mulitiplication {
         System.out.print("Enter number:");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        System.out.printf("Multiplication of entered number %d is: \n",number);
-        for(int i=1;i<=10;i++){
-            System.out.printf("%d * %d =%d \n",number,i,number*i);
+        System.out.printf("Multiplication of entered number %d is: \n", number);
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d =%d \n", number, i, number * i);
         }
+        sc.close();
     }
 }
