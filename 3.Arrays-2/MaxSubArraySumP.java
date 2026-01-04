64aad2d3f616613a67623eb885a93025bde7747e
Harsha <k.harsha0719@gmail.com>
2026-01-04 10:37:18 +0530

    maximum subarray sum

--- 3.Arrays-2/MaxSubArraySumP.java
+++ 3.Arrays-2/MaxSubArraySumP.java
@@ -1,24 +1,27 @@
 
-//Time complexity-O(n^3)
-//Space complexity -O(1)
+//Time complexity-O(n^2)
+//Space complexity -O(n)
 
-public class MaxSubArraySumB {
+public class MaxSubArraySumP {
     public static void getMaxSubArraySum(int[] numbers) {
         int n = numbers.length;
         int currentSum;
         int maxSum = Integer.MIN_VALUE;
+        int[] prefix = new int[n];
+        prefix[0] = numbers[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                currentSum = 0;
-                for (int k = i; k <= j; k++) {
-                    currentSum += numbers[k];
-                }
-                System.out.println(currentSum);
+
+                currentSum = i == 0 ? prefix[i] : prefix[j] - prefix[i - 1];
+
                 if (currentSum > maxSum) {
                     maxSum = currentSum;
                 }
             }
-            System.out.println();
         }
         System.out.println("Maximum subarray sum is :" + maxSum);
     }
