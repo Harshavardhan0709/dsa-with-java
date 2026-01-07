f7330010994fe357ec585d48d0cac4813a0b4239
Harsha <k.harsha0719@gmail.com>
2026-01-07 23:17:15 +0530

    Sorting

---
+++ 3.Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[]={1,5,4,9,0,-1,-100};
+        bubbleSort(nums);
+        for(int a:nums){
+            System.out.print(a+" ");
+        }
+    }
+}
