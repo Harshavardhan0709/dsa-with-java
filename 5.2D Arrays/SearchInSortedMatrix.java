d5b8efef8200f46d7128daebf9bdb78c1f173bfa
Harsha <k.harsha0719@gmail.com>
2026-01-12 12:17:08 +0530

    2D Arrays

--- 5.2D Arrays/SearchInSortedMatrix.java
+++ 5.2D Arrays/SearchInSortedMatrix.java
@@ -16,26 +16,52 @@ public class SearchInSortedMatrix {
     // System.out.println("Not found");
     // return false;
     // }
-    public static boolean rowWiseBinarySearch(int matrix[][], int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        for (int i = 0; i < n; i++) {
-            int low = 0, high = m - 1;
-            while (low <= high) {
-                int mid = (low + high) / 2;
-                if (matrix[i][mid] == target) {
-                    System.out.println("found at (" + i + "," + mid + ")");
-                    return true;
-                } else if (matrix[i][mid] > target) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;
-                }
-            }
-        }
-        System.out.println("Not found");
-        return false;
-    }
+
+    // // Time Complexity:O(nlogm)
+    // // Space Complexity:O(1)
+    // public static boolean rowWiseBinarySearch(int matrix[][], int target) {
+    // int n = matrix.length;
+    // int m = matrix[0].length;
+    // for (int i = 0; i < n; i++) {
+    // int low = 0, high = m - 1;
+    // while (low <= high) {
+    // int mid = (low + high) / 2;
+    // if (matrix[i][mid] == target) {
+    // System.out.println("found at (" + i + "," + mid + ")");
+    // return true;
+    // } else if (matrix[i][mid] > target) {
+    // high = mid - 1;
+    // } else {
+    // low = mid + 1;
+    // }
+    // }
+    // }
+    // System.out.println("Not found");
+    // return false;
+    // }
+
+    // Time Complexity:O(mlogn)
+    // Space Complexity:O(1)
+    // public static boolean colWiseBinarySearch(int matrix[][], int target) {
+    //     int n = matrix.length;
+    //     int m = matrix[0].length;
+    //     for (int j = 0; j < m; j++) {
+    //         int low = 0, high = n - 1;
+    //         while (low <= high) {
+    //             int mid = (low + high) / 2;
+    //             if (matrix[j][mid] == target) {
+    //                 System.out.println("found at (" + j + "," + mid + ")");
+    //                 return true;
+    //             } else if (matrix[j][mid] > target) {
+    //                 high = mid - 1;
+    //             } else {
+    //                 low = mid + 1;
+    //             }
+    //         }
+    //     }
+    //     System.out.println("Not found");
+    //     return false;
+    // }
 
     public static void main(String[] args) {
         int arr[][] = { { 10, 20, 30, 40 },
@@ -47,5 +73,7 @@ public class SearchInSortedMatrix {
         // searchBrute(arr, 3);
         // rowWiseBinarySearch(arr, 33);
         // rowWiseBinarySearch(arr, 3);
+        // colWiseBinarySearch(arr, 33);
+        // colWiseBinarySearch(arr, 3);
     }
 }
